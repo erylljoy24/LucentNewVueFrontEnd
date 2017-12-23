d2820883fec2d3bb4f7ca0c6d8c9e2dcf0380f43
Eryll <eryll.salamanes24@gmail.com>
2017-12-23 19:52:19 +0700

    - added layout for main dashboard and login form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,90 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+  	<!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <button class="btn btn-link nav-link" v-on:click.prevent="navigateTo('/')">
+                Home
+              </button>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item" data-toggle="modal" data-target="#exampleModal">
+              <button class="btn btn-link nav-link">
+                Login
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form class="container">
+              <div class="form-group">
+                <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input type="checkbox" class="form-check-input">
+                  Remember me
+                </label>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  methods:{
+    navigateTo: function (nav) {
+      // Do what you want here.
+      // this.saveTheThing()
+      console.log(nav)
+      this.$router.push({
+          path: nav
+      })
+    }
+  }
 }
 </script>
 
@@ -18,6 +95,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+},
+.form-check-label{
+  padding-left: 1.25rem;
+  margin-bottom: 0;
+  display: inherit;
+  text-align: justify;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import DashboardComponent from '@/components/DashboardComponent'
+import ArticleItemComponent from '@/components/ArticleItemComponent'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+    	path: '/',
+    	name: 'DashboardComponent',
+    	component: DashboardComponent
+    },
+    {
+        path: '/ArticleItemComponent',
+        name: 'ArticleItemComponent',
+        component: ArticleItemComponent
     }
   ]
 })
